a8069768b7340a0de784ae5060c8751b801aadfd
drleovasconcelos <lfreire1985@gmail.com>
2024-07-29 10:44:32 -0300

    quinta alteração

--- scripts.js
+++ scripts.js
@@ -1,79 +1,85 @@
-// resumo interessante
-// const input = document.querySelector('.input')
-// const container = document.querySelector('.container')
-// const addButton = document.querySelector('.add')
+// como usar o lcal storage ... || ou 
+// JSON.parse converte string em objeto
+const todos = JSON.parse(localStorage.getItem('todos')) || []
 
-// function addTarefa() {
-//     alert('Deu certo')
-// }
 
-// addButton.addEventListener('click', addTarefa)
-// // para executar eventos após apertar o botão (todos os eventos) - quando o evento for o click ele chama a função addTarefa
-
+// setItem = serve para salvar / todos = chave / correr e nadar = o que será salvo na chave
+// JSON.stringify(localStorage.setItem('todos', "['correr', 'nadar']"))
+// console.log(todos)
 
 const input = document.querySelector('.input')
 const container = document.querySelector('.container')
 const addButton = document.querySelector('.add')
 
-function addTarefa() {
+
+function salvarTarefas() {
+    localStorage.setItem('todos', JSON.stringify(todos))
+}
+
+function addTarefa(nome) {
     const itemBox = document.createElement('div')
     itemBox.classList.add('item')
-    // tá cirando a div (imagina criando a divi dentro do html e adicionando a classe item - <div class="item"> </div>)
 
     const inputTask = document.createElement('input')
     inputTask.type = 'text'
     inputTask.disabled = true
-    // só vai poder ver o texto e não poderá editar
-    inputTask.value = input.value
-    // o input será o valor que ele digitou lá em cima e em seguida adicionamos as caracteristicas do CSS
+    inputTask.value = nome
     inputTask.classList.add('item-input')
 
     const editBtn = document.createElement('button')
     editBtn.classList.add('edit')
     editBtn.innerText = 'EDITAR'
-    // CRIEI O BOTÃO, E ADICIONEI AS CARACT DO CSS
-    editBtn.addEventListener('click', () => editTarefa(inputTask, input.value))
-    // ao clicar em editar no botão editar, ele recebe os inputs da tarefa
-
+    editBtn.addEventListener('click', () => editTarefa(inputTask, nome))
 
 
     const removeBtn = document.createElement('button')
     removeBtn.classList.add('remove')
     removeBtn.innerText = 'REMOVER'
-    // CRIEI O BOTÃO, E ADICIONEI AS CARACT DO CSS
-    removeBtn.addEventListener('click', () => removeTarefa (itemBox, input.value))
-    // ao clicar em remover no botão editar, ele deleta
+    removeBtn.addEventListener('click', () => removeTarefa(itemBox, nome))
 
-
-
-    // primeiro div dentro do container
     container.append(itemBox)
     itemBox.append(inputTask)
     itemBox.append(editBtn)
     itemBox.append(removeBtn)
 
-    input.value = " "
-    // limpa a caixa de texto
+    todos.push(nome)
+    
+
+    input.value = ''
     input.focus()
-    // clicou no botão o foco volta para o input
 }
 
 function editTarefa(input, tarefa) {
     input.disabled = !input.disabled
+
+    if (!input.disabled) {
+        const index = todos.indexOf(tarefa)
+        todos[index] = input.value
+        salvarTarefas()
+    }
 }
 
 function removeTarefa(itemBox, tarefa) {
-    // container.remove(itemBox) - remove tudo - cuidado
-    // itemBox.parentNode.removeChild(itemBox)
     container.removeChild(itemBox)
+    const index = todos.indexOf(tarefa)
+    todos.splice(index, 1)
+    salvarTarefas()
+   
 }
 
-addButton.addEventListener('click', addTarefa)
-// para executar eventos após apertar o botão (todos os eventos) - quando o evento for o click ele chama a função addTarefa
 
-// chamar função com enter
+addButton.addEventListener('click', () => {
+    addTarefa(input.value)
+    salvarTarefas()
+})
 input.addEventListener('keydown', (e) => {
     if (e.key === 'Enter') {
-        addTarefa()
+        addTarefa(input.value)
+        salvarTarefas()
     }
 })
+
+
+todos.forEach(task => {
+    addTarefa(task)
+});
